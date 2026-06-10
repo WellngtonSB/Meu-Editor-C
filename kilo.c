6c88c80c81577d484b0c39689d1b1ba7717ebfa0
WellngtonSB <wellington.souzask8@gmail.com>
2026-06-09 21:36:46 -0300

    Finalizado o Capítulo 2: Modo Raw e estrutura base

---
+++ kilo.c
@@ -0,0 +1,44 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+
+}
+void enableRawMode(){
+    
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit (disableRawMode);
+    
+    struct termios raw = orig_termios;
+    raw.c_oflag &= ~(OPOST);
+    raw.c_iflag &= ~(IXON | BRKINT | INPCK | ISTRIP);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc [VMIN] = 0;
+    raw.c_cc [VTIME] = 1;
+
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+int main (){
+    enableRawMode();
+
+    char c;
+        while (1){
+            c = 0;
+            read(STDIN_FILENO, &c, 1);
+                if (c == 'q')
+                break;
+        }
+
+
+    return 0;
+
+}
